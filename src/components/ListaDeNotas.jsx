5a2432f52e4bb4ec423b5b3f888087845248b690
Daniel Gomes <danielgomes@dginfope.com.br>
2020-08-10 23:21:41 -0300

    Primeiro commit

---
+++ src/components/ListaDeNotas.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+export default class ListaDeNotas extends Component {
+
+    render() {
+        return (
+            <ul>
+                <li>
+                    <section>
+                        <header>Título</header>
+                        <p>Escreva sua nota</p>
+                    </section>
+                </li>
+                <li>
+                    <section>
+                        <header>Título</header>
+                        <p>Escreva sua nota</p>
+                    </section>
+                </li>
+                <li>
+                    <section>
+                        <header>Título</header>
+                        <p>Escreva sua nota</p>
+                    </section>
+                </li>
+            </ul>
+        );
+    }
+}
