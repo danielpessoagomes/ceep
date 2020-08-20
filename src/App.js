0c1e6f2d068acabc033f935c51b009d1c00b9c85
Daniel Gomes <danielgomes@dginfope.com.br>
2020-08-19 23:12:03 -0300

    Conclusão CEEP alura

--- src/App.js
+++ src/App.js
@@ -6,11 +6,27 @@ import "./assets/index.css";
 
 class App extends Component {
 
+  constructor() {
+    super();
+    this.state = {
+      notas: []
+    }
+  }
+
+  criarNota(titulo, texto) {
+    const novaNota = { titulo, texto };
+    const novoArrayDeNotas = [...this.state.notas, novaNota]
+    const novoEstado = {
+      notas: novoArrayDeNotas
+    }
+    this.setState(novoEstado);
+  }
+
   render() {
     return (
       <section className="conteudo">
-        <FormularioCadastro />
-        <ListaDeNotas />
+        <FormularioCadastro criarNota={this.criarNota.bind(this)} />
+        <ListaDeNotas notas={this.state.notas} />
       </section>
     );
   }
